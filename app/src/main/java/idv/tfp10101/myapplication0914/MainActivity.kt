bf822f2e35d674a5430b60dd94755cf0cb8dafe1
陳柏文 <bbtiger0826@gmail.com>
2021-09-14 09:51:05 +0800

    本機端使用方式

--- app/src/main/java/idv/tfp10101/myapplication0914/MainActivity.kt
+++ app/src/main/java/idv/tfp10101/myapplication0914/MainActivity.kt
@@ -2,10 +2,13 @@ package idv.tfp10101.myapplication0914
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import idv.tfp10101.mysdk0914.TestOne
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TestOne.testOne()
     }
 }
---
+++ mysdk0914/src/main/java/idv/tfp10101/mysdk0914/TestOne.kt
@@ -0,0 +1,9 @@
+package idv.tfp10101.mysdk0914
+
+import android.util.Log
+
+object TestOne {
+    fun testOne() {
+        Log.d("bbtig", "test_one")
+    }
+}
